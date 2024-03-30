c9f0930fadb0ae88cd3f21d63d66b56683ee7213
albinramos <dramosh79@gmail.com>
2024-03-30 17:29:08 +0100

    register

--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const userRoutes = require('./routes/userRoutes');
 const app = express();
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((error) => {
-  console.log(error);
-});
+app.use('/api/auth', userRoutes);
 
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
 
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port: ${server.address().port}`);
-});  
+    // Inicia el servidor después de conectarse a MongoDB
+    const server = app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port: ${server.address().port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB', error);
+  });
