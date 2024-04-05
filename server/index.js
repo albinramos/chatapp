2385f74143a0bcc6c09b9f175d71cbf5d73001ea
albinramos <dramosh79@gmail.com>
2024-04-05 13:00:55 +0200

    server complete

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 const messageRoute = require('./routes/messagesRoutes');
 const app = express();
+const socket = require('socket.io');
 require('dotenv').config();
 
 app.use(cors());
@@ -19,6 +20,29 @@ mongoose.connect(process.env.MONGO_URL)
     // Inicia el servidor después de conectarse a MongoDB
     const server = app.listen(process.env.PORT, () => {
       console.log(`Server is running on port: ${server.address().port}`);
+    });
+
+    const io = socket(server, {
+      cors: {
+        origin: 'http://localhost:3000',
+        credentials: true,
+      },
+    });
+
+    global.onlineUsers = new Map();
+
+    io.on("connection", (socket) => {
+      global.chatSocket = socket;
+      socket.on("add-user", (userId) => {
+        onlineUsers.set(userId, socket.id);
+      });
+
+      socket.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if(sendUserSocket) {
+          socket.to(sendUserSocket).emit("msg-receive", data.message);
+        }
+      });
     });
   })
   .catch((error) => {
